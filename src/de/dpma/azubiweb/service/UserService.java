964a7695b15ed59659b6c9dcf6e2709af45fc0ed
Kenneth Böhmer <Krixjun@hotmail.de>
2018-03-29 13:45:51 +0200

    JavaDoc hinzugefügt, Merge conflict
    Kenneth Böhmer

--- src/de/dpma/azubiweb/service/UserService.java
+++ src/de/dpma/azubiweb/service/UserService.java
@@ -11,10 +11,14 @@ import de.dpma.azubiweb.model.User;
 import de.dpma.azubiweb.model.UserRepository;
 import de.dpma.azubiweb.util.PasswordAuthentication;
 
-//@Controller
-//@RequestMapping("../model")
-//@Component
-//@RestController
+/**
+ * Kommuniziert mit {@link UserRepository} und muss verwendet werden, wenn man
+ * mit der Datenbank kommunizieren möchte. <br>
+ * Es ist der Controller für JPA.
+ * 
+ * @author Kenneth Böhmer
+ *
+ */
 @Service
 public class UserService {
 	
@@ -23,12 +27,33 @@ public class UserService {
 	
 	private PasswordAuthentication pa = new PasswordAuthentication();
 	
+	/**
+	 * Speichert einen Benutzer.
+	 * 
+	 * @param user
+	 *            Benutzer
+	 */
 	public void saveUser(User user) {
 		
 		user = checkUser(user);
 		userRepository.save(user);
 	}
 	
+	/**
+	 * Überprüft ob das richtige Passwort eingegeben wurde für den Benutzer.
+	 * <br>
+	 * Das Passwort wird über {@link PasswordAuthentication} verschlüsselt und
+	 * verglichen. <br>
+	 * Wirft einen Fehler zur {@link Session} bzw. {@link UserSession}, falls
+	 * der Benutzer nicht gefunden wurde.
+	 *
+	 * @param username
+	 *            Benutzername
+	 * @param password
+	 *            Passwort
+	 * @return <strong>boolean</strong> true wenn das Passwort mit dem in der
+	 *         Datenbank übereinstimmt.
+	 */
 	public boolean validateUser(String username, String password) {
 		
 		if (getUserByName(username) == null || getUserByName(username).getUsername().isEmpty()) {
@@ -40,16 +65,39 @@ public class UserService {
 		return false;
 	}
 	
+	/**
+	 * Holt sich den Benutzer über den Benutzernamen.
+	 * 
+	 * @param username
+	 *            Benutzernamen
+	 * @return den Benutzer
+	 */
 	public User getUserByName(String username) {
 		
 		return userRepository.findByUsernameIgnoreCase(username);
 	}
 	
+	/**
+	 * Holt sich den Benutzer über die ID.
+	 * 
+	 * @param id
+	 * @return den Benutzer
+	 */
 	public User getUserByID(int id) {
 		
 		return userRepository.findById(id);
 	}
 	
+	/**
+	 * Aktuallisiert den Benutzer. <br>
+	 * Momentan wird der Benutzer nur mit der {@link UserRepository#save(User)
+	 * save(User)} Methode von {@link UserRepository} aufgerufen und kein echtes
+	 * Update Befehl. <br>
+	 * Vom übergebenen Benutzer werden {@link UserService#checkUser(User) die
+	 * fehlenden Daten ergänzt} und das {@link PasswordAuthentication Passwort}
+	 * verschlüsselt.
+	 * 
+	 */
 	// TODO Richtiges Update statement ausführen
 	// TODO Überprüfen ob die einfache save() Methode doch möglich ist
 	@SuppressWarnings("Nicht Benutzbar")
@@ -79,18 +127,43 @@ public class UserService {
 		}
 	}
 	
+	/**
+	 * Setzt den Benutzernamen und E-Mail, falls sie nicht übergeben wurden.<br>
+	 * Verschlüsselt das {@link PasswordAuthentication Passwort}.
+	 * 
+	 * @param user
+	 * @return
+	 */
 	private User checkUser(User user) {
 		
+		String vorname = user.getVorname();
+		String nachname = user.getNachname();
+		vorname = correctName(vorname);
+		nachname = correctName(nachname);
+		
 		if (user.getUsername() == null || user.getUsername().trim().isEmpty())
-			user.setUsername(user.getVorname().substring(0, 2)
-					+ (user.getNachname().length() <= 6 ? user.getNachname() : user.getNachname().substring(0, 6)));
+			user.setUsername(vorname.substring(0, 2) + (nachname.length() <= 6 ? nachname : nachname.substring(0, 6)));
 		if (user.getEmail() == null || user.getEmail().isEmpty())
-			user.setEmail(user.getVorname() + "." + user.getNachname() + "@dpma.de");
+			user.setEmail(vorname + "." + nachname + "@dpma.de");
 		if (!pa.isHashed(user.getPassword()))
 			user.setPassword(pa.hash(user.getPassword().toCharArray()));
 		return user;
 	}
+	private String correctName(String name) {
+		
+		name = name.replaceAll("ä", "ae");
+		name = name.replaceAll("ö", "oe");
+		name = name.replaceAll("ü", "ue");
+		return name;
+	}
 	
+	/**
+	 * Löscht einen Benutzer aus der Datenbank.
+	 * 
+	 * @param user
+	 *            Benutzer
+	 * @return true == Falls keine Exception geworfen wurde.
+	 */
 	public boolean deleteUser(User user) {
 		
 		try {
@@ -102,6 +175,13 @@ public class UserService {
 		}
 	}
 	
+	/**
+	 * Löscht einen Benutzer aus der Datenbank.
+	 * 
+	 * @param id
+	 *            BenutzerId
+	 * @return true == Falls keine Exception geworfen wurde.
+	 */
 	public boolean deleteUserById(int id) {
 		
 		try {
@@ -113,6 +193,13 @@ public class UserService {
 		}
 	}
 	
+	/**
+	 * Löscht einen Benutzer aus der Datenbank.
+	 * 
+	 * @param username
+	 *            Benutzername
+	 * @return true == Falls keine Exception geworfen wurde.
+	 */
 	public boolean deleteUserByUsername(String username) {
 		
 		try {
@@ -124,6 +211,15 @@ public class UserService {
 		}
 	}
 	
+	/**
+	 * Ruft die {@link UserRepository#updateUserPassword(int, String) } auf.
+	 * <br>
+	 * Hashed das Passwort und speichert es in die Datenbank.
+	 * 
+	 * @param user
+	 *            Benutzer mit dem Aktuallisierten Passwort
+	 * @return true == Falls keine Exception geworfen wurde.
+	 */
 	public boolean updateUserPasswort(User user) {
 		
 		try {
@@ -137,11 +233,23 @@ public class UserService {
 		}
 	}
 	
+	/**
+	 * Holt sich alle Benutzer aus der Datenbank.
+	 * 
+	 * @return Alle Benutzer
+	 */
 	public List<User> getAllUser() {
 		
 		return userRepository.findAll();
 	}
 	
+	/**
+	 * Holt sich alle Benutzer aus der Datenbank mit der gleichen Rolle.
+	 * 
+	 * @param rolle
+	 *            Rolle
+	 * @return Alle Benutzer mit der gleichen Rolle
+	 */
 	public List<User> getUserByRolle(Rolle rolle) {
 		
 		return userRepository.findByRolle(rolle);
