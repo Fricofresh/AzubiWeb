3f35b6c4cb6754dbd04e311f77b4c99bf20436b0
Kenneth Böhmer <Krixjun@hotmail.de>
2018-08-10 16:53:39 +0200

    Merged

--- src/de/dpma/azubiweb/view/RootPage.java
+++ src/de/dpma/azubiweb/view/RootPage.java
@@ -2,6 +2,7 @@ package de.dpma.azubiweb.view;
 
 import java.text.ParseException;
 
+import org.apache.wicket.AttributeModifier;
 import org.apache.wicket.Component;
 import org.apache.wicket.Session;
 import org.apache.wicket.markup.html.WebMarkupContainer;
@@ -15,23 +16,7 @@ import de.dpma.azubiweb.InitialInsert;
 import de.dpma.azubiweb.model.Rolle.Beschreibung;
 import de.dpma.azubiweb.model.User;
 import de.dpma.azubiweb.service.UserSession;
-
-
-import java.text.ParseException;
-
-import org.apache.wicket.Component;
-import org.apache.wicket.Session;
-import org.apache.wicket.markup.html.WebMarkupContainer;
-import org.apache.wicket.markup.html.WebPage;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.Link;
-import org.apache.wicket.model.Model;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-
-import de.dpma.azubiweb.InitialInsert;
-import de.dpma.azubiweb.model.Rolle.Beschreibung;
-import de.dpma.azubiweb.model.User;
-import de.dpma.azubiweb.service.UserSession;
+import de.dpma.azubiweb.util.AlertUtil;
 
 /**
  * Stellt das Grundgerüst für alle Seiten da. <br>
@@ -61,6 +46,8 @@ public class RootPage extends WebPage {
 	 */
 	protected Label titelLabel = new Label("titelLabel", Model.of(this.getClass().getSimpleName()));
 	
+	private Label alertMessage;
+	
 	/**
 	 * @see #initial
 	 */
@@ -69,7 +56,8 @@ public class RootPage extends WebPage {
 		super();
 		try {
 			initial();
-		} catch (ParseException e) {
+		}
+		catch (ParseException e) {
 			e.printStackTrace();
 		}
 	}
@@ -77,14 +65,15 @@ public class RootPage extends WebPage {
 	/**
 	 * @see #initial
 	 * @param params
-	 * @throws ParseException 
+	 * @throws ParseException
 	 */
-	public RootPage(final PageParameters params)  {
+	public RootPage(final PageParameters params) {
 		
 		super(params);
 		try {
 			initial();
-		} catch (ParseException e) {
+		}
+		catch (ParseException e) {
 			e.printStackTrace();
 		}
 	}
@@ -92,7 +81,8 @@ public class RootPage extends WebPage {
 	/**
 	 * Setzt die Funktionen die einzelnen Punkte in der Navigationsleiste. <br>
 	 * Initialisiert alle {@link Component}
-	 * @throws ParseException 
+	 * 
+	 * @throws ParseException
 	 */
 	private void initial() throws ParseException {
 		
@@ -104,32 +94,106 @@ public class RootPage extends WebPage {
 		
 		navLeisteWebMarkupContainer = new WebMarkupContainer("navLeisteWebMarkupContainer");
 		
-		benutzerVerwaltungWebMarkupContainer = new WebMarkupContainer("benutzerVerwaltungWebMarkupContainer");
+		addBenutzerVerwaltung();
+		addAbmelden();
+		addEinstellungen();
+		addBerichtsheft();
+		addSteckbrief();
+		addEinsatzplan();
 		
-		benutzerVerwaltungWebMarkupContainer.add(new Link<String>("benutzerAnlageLink") {
+		add(navLeisteWebMarkupContainer);
+		
+		addAlertMessage();
+		
+		add(titelLabel);
+		
+		navLeisteWebMarkupContainer.setVisible(session.isSignedIn());
+	}
+	
+	private void addAlertMessage() {
+		
+		WebMarkupContainer alertMessageParent = new WebMarkupContainer("alertMessageParent", Model.of());
+		alertMessage = new Label("alertMessage", Model.of());
+		alertMessageParent.add(alertMessage);
+		add(alertMessageParent);
+	}
+	
+	protected void setAlert(AlertUtil.AlertType alertType, String message) {
+		
+		alertMessage.add(new AttributeModifier("class", new Model() {
+			
+			public Object getObject(final Component component) {
+				
+				String cssClass = AlertUtil.getCss(alertType);
+				
+				return cssClass;
+			}
+		}));
+		alertMessage.setDefaultModel(Model.of(message));
+	}
+	
+	private void addEinsatzplan() {
+		
+		navLeisteWebMarkupContainer.add(new Link<String>("einsatzplanLink") {
 			
 			private static final long serialVersionUID = 1L;
 			
 			@Override
 			public void onClick() {
 				
-				setResponsePage(BenutzerAnlage.class);
+				// TODO Do Something
 			}
 		});
-		benutzerVerwaltungWebMarkupContainer.add(new Link<String>("benutzerBearbeitenLink") {
+	}
+	
+	private void addSteckbrief() {
+		
+		navLeisteWebMarkupContainer.add(new Link<String>("steckbriefLink") {
 			
 			private static final long serialVersionUID = 1L;
 			
 			@Override
 			public void onClick() {
 				
-				setResponsePage(BenutzerListe.class);
+				// TODO Do Something
+				
 			}
 		});
+	}
+	
+	private void addBerichtsheft() {
 		
-		navLeisteWebMarkupContainer.add(benutzerVerwaltungWebMarkupContainer);
-		if (user != null && user.getRolle().getId() != Beschreibung.AL.getRolleId())
-			benutzerVerwaltungWebMarkupContainer.setVisible(false);
+		navLeisteWebMarkupContainer.add(new Link<String>("berichtsheftLink") {
+			
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void onClick() {
+				
+				// TODO Do Something
+			}
+		});
+	}
+	
+	private void addEinstellungen() {
+		
+		navLeisteWebMarkupContainer.add(new Link<String>("einstellungenLink") {
+			
+			/**
+			 * 
+			 */
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void onClick() {
+				
+				setResponsePage(Einstellungen.class);
+			}
+		});
+	}
+	
+	private void addAbmelden() {
+		
 		navLeisteWebMarkupContainer.add(new Link<String>("abmeldenLink") {
 			
 			/**
@@ -144,54 +208,48 @@ public class RootPage extends WebPage {
 				setResponsePage(LoginPage.class);
 			}
 		});
-		navLeisteWebMarkupContainer.add(new Link<String>("einstellungenLink") {
-			
-			/**
-			 * 
-			 */
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			public void onClick() {
-				
-				setResponsePage(Einstellungen.class);
-			}
-		});
-		navLeisteWebMarkupContainer.add(new Link<String>("berichtsheftLink") {
-			
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			public void onClick() {
-				
-				// TODO Do Something
-			}
-		});
-		add(navLeisteWebMarkupContainer);
-		navLeisteWebMarkupContainer.add(new Link<String>("steckbriefLink") {
-			
-			private static final long serialVersionUID = 1L;
-			
-			@Override
-			public void onClick() {
-				
-				setResponsePage(Steckbrief.class);
-				
-			}
-		});
+	}
+	
+	private void addBenutzerVerwaltung() {
 		
-		navLeisteWebMarkupContainer.add(new Link<String>("einsatzplanLink") {
+		benutzerVerwaltungWebMarkupContainer = new WebMarkupContainer("benutzerVerwaltungWebMarkupContainer");
+		
+		addBenutzerAnlage();
+		
+		benutzerBearbeiten();
+		
+		navLeisteWebMarkupContainer.add(benutzerVerwaltungWebMarkupContainer);
+		
+		if (user != null && user.getRolle().getId() != Beschreibung.AL.getRolleId())
+			benutzerVerwaltungWebMarkupContainer.setVisible(false);
+	}
+	
+	private void benutzerBearbeiten() {
+		
+		benutzerVerwaltungWebMarkupContainer.add(new Link<String>("benutzerBearbeitenLink") {
 			
 			private static final long serialVersionUID = 1L;
 			
 			@Override
 			public void onClick() {
 				
-				// TODO Do Something
+				setResponsePage(BenutzerListe.class);
 			}
 		});
-		add(titelLabel);
-		navLeisteWebMarkupContainer.setVisible(session.isSignedIn());
+	}
+	
+	private void addBenutzerAnlage() {
+		
+		benutzerVerwaltungWebMarkupContainer.add(new Link<String>("benutzerAnlageLink") {
+			
+			private static final long serialVersionUID = 1L;
+			
+			@Override
+			public void onClick() {
+				
+				setResponsePage(BenutzerAnlage.class);
+			}
+		});
 	}
 	
 	/**
