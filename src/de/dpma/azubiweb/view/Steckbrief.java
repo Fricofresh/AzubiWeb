421ddc9cc2566a2f61e9e310a7c35764f15c67fa
Kenneth Böhmer <Krixjun@hotmail.de>
2019-03-17 22:17:17 +0100

    Steckbrief: AL comes now from the db

--- src/de/dpma/azubiweb/view/Steckbrief.java
+++ src/de/dpma/azubiweb/view/Steckbrief.java
@@ -2,22 +2,38 @@ package de.dpma.azubiweb.view;
 
 import java.util.List;
 
-import de.dpma.azubiweb.model.Rolle;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.spring.injection.annot.SpringBean;
+
 import de.dpma.azubiweb.model.Rolle.Beschreibung;
 import de.dpma.azubiweb.model.User;
+import de.dpma.azubiweb.service.RolleService;
+import de.dpma.azubiweb.service.UserService;
 
-public class Steckbrief extends BenutzerVerwaltungsBasePage {
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-
-public Steckbrief() {
-super();
-List<User> ausbildungsleiter = userService.getUserByRolle(rolleService.getRolle(Beschreibung.AL));
-
-
-
-}
+public class Steckbrief extends RootPage {
+	
+	@SpringBean
+	private UserService userService;
+	
+	@SpringBean
+	private RolleService rolleService;
+	
+	private static final long serialVersionUID = 4658861470984188261L;
+	
+	public Steckbrief() {
+		
+		super();
+		List<User> ausbildungsleiter = userService.getUserByRolle(rolleService.getRolle(Beschreibung.AL));
+		ListView<User> ausbildungsleiterListView = new ListView<User>("ausbildungsleiterListView", ausbildungsleiter) {
+			
+			private static final long serialVersionUID = 8602112233927576927L;
+			
+			@Override
+			protected void populateItem(ListItem<User> item) {
+				
+				// User ausbildungsleiter = item.getModelObject();
+			}
+		};
+	}
 }
